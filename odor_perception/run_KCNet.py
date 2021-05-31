f877a3fbdbc6dc92465bdcd2f254d6eb0175768e
jinyunghong <jhong53@asu.edu>
2021-05-31 11:36:18 +0900

    Upload run_KCNet.py for odor perception

---
+++ odor_perception/run_KCNet.py
@@ -0,0 +1,49 @@
+import argparse
+import pandas as pd
+from models.KCNet import KCNet
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import time
+from imblearn.over_sampling import ADASYN
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data', type=str, default=None, help='Dataset.', required=True)
+parser.add_argument('--hsize', type=int, default=1000, help='The number of hidden nodes.')
+opt = parser.parse_args()
+
+# Dataset
+if opt.data == 'sweet':
+    print('Dataset : Sweet')
+    PATH_DATA_ODOR_SWEET = '~/data/odor_perception/odor_sweet_dataset.csv'
+    df_sweet = pd.read_csv(PATH_DATA_ODOR_SWEET)
+    X, y = df_sweet[df_sweet.columns[:-1]].to_numpy(), df_sweet[df_sweet.columns[-1]].to_numpy()
+    X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.1, random_state=1, stratify=y)
+    sc = StandardScaler()
+    X_train_val = sc.fit_transform(X_train_val)
+    X_test = sc.transform(X_test)
+    alpha = 100
+else:
+    print('Dataset : Musky')
+    PATH_DATA_ODOR_MUSKY = "~/data/odor_perception/odor_musky_dataset.csv"
+    df_musky = pd.read_csv(PATH_DATA_ODOR_MUSKY)
+    X, y = df_musky[df_musky.columns[:-1]].to_numpy(), df_musky[df_musky.columns[-1]].to_numpy()
+    # define oversampling strategy
+    oversample = ADASYN(random_state=1)
+    X_over, y_over = oversample.fit_resample(X, y)
+    X_train_val, X_test, y_train_val, y_test = train_test_split(X_over, y_over, test_size=0.1, random_state=1)
+    alpha = 115
+# apply standard normalization to dataset
+sc = StandardScaler()
+X_train_val = sc.fit_transform(X_train_val)
+X_test = sc.transform(X_test)
+
+start_time = time.time()
+print("Model training w/ hsize: {}".format(opt.hsize))
+model = KCNet(input_size=159, h_size=opt.hsize, alpha=alpha)
+model.fit(X_train_val, y_train_val)
+preds = model.predict_label(X_test)
+cr = classification_report(preds, y_test, output_dict=True)
+
+print('time : {} sec'.format(time.time() - start_time))
+print('Weighted F1 score: {}'.format(cr['weighted avg']['f1-score']))
